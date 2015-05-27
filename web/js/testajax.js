2122c81610769861959c3fe8843f004f92a2678a
Barack Hussein <b.h.obama-2@yandex.ru>
2015-05-27 10:11:01 +0300

    add ajax comment

--- web/js/testajax.js
+++ web/js/testajax.js
@@ -1,4 +1,27 @@
+function onPagesKeyClick(event) {
+  var target = event.target;
+  var fileKey = filekey.getAttribute('value');
+  var oldDiv = document.getElementById('allcomments');
+  var id = target.getAttribute('data-id');
+  if (!id) {
+    return;
+  }
+  else {
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", "http://localhost/filehost/web/ajax/" + fileKey + "/" + id, true);
+    xmlHttp.onreadystatechange = function () {
+      if (xmlHttp.readyState == 4) {
+        if (xmlHttp.status == 200) {
+          oldDiv.innerHTML = xmlHttp.responseText;
+        }
+        else {
+          alert('Error');
+        }
+      }
+    };
 
-  var filekey=filekey.getAttribute('value');
-  xmlHttp = new XMLHttpRequest();
-  xmlHttp.open("GET", "http://localhost/filehost/web/ajax/"+fileKey, true);
+    xmlHttp.send(null);
+  }
+}
+
+document.addEventListener("click", onPagesKeyClick);
